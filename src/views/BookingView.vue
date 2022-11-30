7f542165f92f5199183d04e32824f5f5428b4878
Louis Adibe <21458812@life.hkbu.edu.hk>
2022-11-30 18:48:57 +0800

    Front End Analysis

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="container">
       <ul class="list-group">
         <li class="list-group-item">{{ booking._id }}</li>
@@ -25,42 +24,12 @@ export default {
   components: {
     // HelloWorld
     // PaginatedBookings,
-=======
-  <div class="container">
-    <ul class="list-group">
-      <!-- and whenever we change the id the following items will rfefresh  -->
-      <li class="list-group-item">{{ booking._id }}</li>
-      <li class="list-group-item">{{ booking.email }}</li>
-      <li class="list-group-item">A third item</li>
-      <li class="list-group-item">A fourth item</li>
-      <li class="list-group-item">And a fifth one</li>
-    </ul>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-// import PaginatedBookings from '@/components/PaginatedBookings.vue'
-// The below "useRoute" will help us to access the dynamic parameter
-import{ref,onMounted} from 'vue'
-import { useRoute } from "vue-router";
-export default {
-  name: 'BookingView',
-  components: {
-    // HelloWorld
-    // PaginatedBookings
->>>>>>> 117c001c9f531e67dff6353148ff374f516ed8f3
   },
   setup() {
     const booking = ref({});
     const route = useRoute();
 
     onMounted(async () => {
-<<<<<<< HEAD
-=======
-
->>>>>>> 117c001c9f531e67dff6353148ff374f516ed8f3
       var response = await fetch("/api/bookings/" + route.params.id);
 
       if (response.ok) {
@@ -68,7 +37,6 @@ export default {
       } else {
         alert(response.statusText);
       }
-<<<<<<< HEAD
     });
 
     return {
@@ -76,13 +44,4 @@ export default {
     };
   },
 };
-=======
-    })
-
-    return {
-      booking
-    }
-  }
-}
->>>>>>> 117c001c9f531e67dff6353148ff374f516ed8f3
 </script>
